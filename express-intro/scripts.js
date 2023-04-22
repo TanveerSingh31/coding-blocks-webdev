c928a92edee7f1ffde41f88f0cd6383cca47f3fd
Tanveer Singh <tanveer.singh1@incedoinc.com>
2023-04-22 18:43:15 +0530

    added path parameters

--- express-intro/scripts.js
+++ express-intro/scripts.js
@@ -19,3 +19,16 @@ app.get("/greet", (req,res)=>{
 
     res.send("good morning " + person);
 })
+
+
+
+//                                      path parameters
+
+// "/:city" means that we can enter anything in the url and it will be saved in the city variable
+// example => http://localhost:3000/mohali/welcome 
+
+app.get("/:city/:greeting", (req,res)=>{
+    let city = req.params.city;
+    let greet = req.params.greeting;
+    res.send(`${greet} to ${city}`);
+})
