f4d607ee9b4ae34cf9859ec1662c31d96757b017
Tanveer Singh <tanveer.singh1@incedoinc.com>
2023-04-23 10:44:20 +0530

    adding explanation of middlewares
    Signed-off-by: Tanveer Singh <tanveer.singh1@incedoinc.com>

--- middlewares/server.js
+++ middlewares/server.js
@@ -5,14 +5,34 @@ const app = express();
 
 //  serves static files to the browser on the home route : "/" 
     
-// app.use("/", express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
+
+// bunch of middlewares
+
+function m1 (req,res,next) {
+    console.log("middleware 1");
+    next();
+}
+function m2 (req,res,next) {
+    console.log("middleware 1");
+    next();
+}
+function m3 (req,res,next) {
+    console.log("middleware 1");
+    next();
+}
+
+/* middlewares are functions that executed when a request comes to the server
+    after the middleware has completed its execution 
+    either "next()" or "res.send()" is to be called to complete req,res cycle
+    next() calls the next middleware in line
+    res.send() sends response to the browser back */
 
 
+// 1st parameter is path to which request comes 
+// all other paramters after it are middlewares , that will execute sequentially
 
-
-
-
-app.get("/", (req,res)=>{
+app.get("/hello", m1, m2, m3, (req,res)=>{
     // req.query prints of all the query parameters values in a json format
     console.log(req.query);
     res.send(`hello ${req.query.name}`);
